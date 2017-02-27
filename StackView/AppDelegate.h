c79dff9abba0480dfb2e9b416308501e31375cca
dlsuresh <dl.suresh6@gmail.com>
2017-02-27 17:35:47 +0530

    Initial Commit

---
+++ StackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackView
+//
+//  Created by Suresh on 2/27/17.
+//  Copyright © 2017 Suresh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
